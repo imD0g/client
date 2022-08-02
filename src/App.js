e526c112890ce645d167dab2ca1720a568fd51ed
imD0g <alphagamingcommunity@gmail.com>
2022-08-02 21:42:47 +0100

    Added basic visual front end

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
 
 function App() {
-    return (
-        <div className="App">
-            <div>
-                <input type="text" />
-            </div>
-        </div>
-    );
+     return (
+          <div className="App">
+               <div className="information">
+                    <label>Name:</label>
+                    <input type="text" />
+                    <label>Age:</label>
+                    <input type="number" />
+                    <label>Country:</label>
+                    <input type="text" />
+                    <label>Position:</label>
+                    <input type="text" />
+                    <label>Wage (Yearly):</label>
+                    <input type="number" />
+                    <button>Add Employee</button>
+               </div>
+          </div>
+     );
 }
 
 export default App;
